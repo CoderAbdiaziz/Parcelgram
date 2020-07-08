5bbaced9df258d0fe03ec9f9ce5d2a19659407c5
Abdiaziz Mohamoud <moha0469@d.umn.edu>
2020-07-08 16:29:26 -0500

    finished home screen
    Users can now see the most recent posts

--- app/src/main/java/Fragments/PostsFragment.java
+++ app/src/main/java/Fragments/PostsFragment.java
@@ -2,21 +2,38 @@ package Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.parcelgram.Post;
+import com.example.parcelgram.PostsAdapter;
 import com.example.parcelgram.R;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link PostsFragment#newInstance} factory method to
+ * Use the {@link PostsFragment# newInstance} factory method to
  * create an instance of this fragment.
  */
 public class PostsFragment extends Fragment {
 
+    public static final String TAG = "PostsFragment";
+    private RecyclerView rvPosts;
+    private PostsAdapter adapter;
+    private List<Post> allPosts;
 
     public PostsFragment() {
         // Required empty public constructor
@@ -29,5 +46,47 @@ public class PostsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_posts, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rvPosts = view.findViewById(R.id.rvPosts);
+
+
+        // steps to use the recycler view:
+        // 0. create layout for one row in the list
+        // 1. create the adapter
+        allPosts = new ArrayList<>();
+
+        adapter = new PostsAdapter(getContext(),allPosts);
+        // 2. create the data source
+        // 3. set the adapter on the recycler view
+
+        rvPosts.setAdapter(adapter);
+        // 4. set the layout manager on the recycler view
+        rvPosts.setLayoutManager(new LinearLayoutManager(getContext()));
+        queryPosts();
+
+    }
+
+    private void queryPosts() {
+        // Specify which class to query
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if (e!=null) {
+                    Log.e(TAG, "Issue with getting posts", e);
+                    return ;
+                }
+                for (Post post : posts) {
+                    Log.i(TAG, "Post: "+ post.getDescription() + ", username: " +post.getUser().getUsername());
+                }
+                allPosts.addAll(posts);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 }
--- app/src/main/java/Fragments/ProfileFragment.java
+++ app/src/main/java/Fragments/ProfileFragment.java
@@ -1,14 +1,21 @@
 package Fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.parcelgram.LoginActivity;
 import com.example.parcelgram.R;
+import com.parse.ParseUser;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -17,6 +24,8 @@ import com.example.parcelgram.R;
  */
 public class ProfileFragment extends Fragment {
 
+    private static final String TAG = "AccountPageActivity";
+    private Button btnLogout;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -29,4 +38,19 @@ public class ProfileFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnLogout = view.findViewById(R.id.btnLogout);
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "onClick login button");
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+            }
+        });
+    }
+
 }

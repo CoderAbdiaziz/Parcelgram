7d1ff5d2eac1c7883610c7e12cf60ec25bed4494
Abdiaziz Mohamoud <moha0469@d.umn.edu>
2020-07-09 18:05:15 -0500

    Centered log card view and made it similar to instagram login page

--- app/src/main/java/com/example/parcelgram/PostsAdapter.java
+++ app/src/main/java/com/example/parcelgram/PostsAdapter.java
@@ -58,10 +58,7 @@ public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.ViewHolder> 
         private TextView tvDescription;
         private  TextView tvUsername2;
 
-        private static final int SECOND_MILLIS = 1000;
-        private static final int MINUTE_MILLIS = 60 * SECOND_MILLIS;
-        private static final int HOUR_MILLIS = 60 * MINUTE_MILLIS;
-        private static final int DAY_MILLIS = 24 * HOUR_MILLIS;
+
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
